f23ce45de9e94d0b4887976e4d9bb0dc3bcc0786
diksha421 <dikshashrma9135@gmail.com>
2025-04-17 12:23:52 +0530

    Add files via upload

---
+++ assign.java
@@ -0,0 +1,13 @@
+//Assignment,increment,dcremenet operator
+import java.util.*;
+public class assign {
+  public static void main(String[] args){
+
+    int x=5;
+    System.out.println(x);
+    x++;
+    System.out.println(x);
+    x--;
+    System.out.println(x);
+  }
+}
